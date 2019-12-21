6ef61d69589e70c70fef3f53c562a555c802ca05
ps-rakeshchintha <rakesh.chintha@neudesic.com>
2019-12-21 13:45:10 +0530

    added canonical tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import { Component, Inject } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { filter } from 'rxjs/operators';
+import { DOCUMENT } from "@angular/common";
 
 @Component({
   selector: 'app-root',
@@ -13,22 +15,51 @@ export class AppComponent {
   showMobileMenuIcon: boolean;
   currentRoute: string;
 
-  constructor(breakpointObserver: BreakpointObserver, router : Router) {
+  constructor(breakpointObserver: BreakpointObserver, router: Router, @Inject(DOCUMENT) private document: Document) {
     breakpointObserver.observe([
       Breakpoints.HandsetLandscape,
       Breakpoints.HandsetPortrait
     ]).subscribe(result => {
       this.showMobileMenuIcon = result.matches ? true : false;
     });
-    router.events.subscribe(val => {
-      if(this.currentRoute !== router.url){
+    router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      if (this.currentRoute !== router.url) {
         this.showMobileMenu = false;
         this.currentRoute = router.url;
+        const link = <HTMLLinkElement>this.document.head.querySelector("link[rel='canonical']")
+          || this.document.head.appendChild(this.document.createElement("link"));
+        link.rel = "canonical";
+        link.href = this.getCanonicalUrl();
       }
     });
   }
 
-  menuClick(){
+  getCanonicalUrl() {
+    let href: string;
+    switch (this.currentRoute) {
+      case "/crop-image":
+        href = "https://easyimgedit.com/crop-image"
+        break;
+      case "/resize-image":
+        href = "https://easyimgedit.com/resize-image"
+        break;
+      case "/compress-jpg":
+        href = "https://easyimgedit.com/compress-jpg"
+        break;
+      case "/404":
+        href = "https://easyimgedit.com/404"
+        break;
+
+      default:
+        href = "https://easyimgedit.com/"
+        break;
+    }
+    return href;
+  }
+
+  menuClick() {
     this.showMobileMenu = !this.showMobileMenu;
   }
 }
